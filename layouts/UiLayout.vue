1b5ea9511924e431dc8e94ab514e67da13be143c
supervueman <supervueman@gmail.com>
2020-09-13 21:43:22 +0400

    Edit layout

--- layouts/UiLayout.vue
+++ layouts/UiLayout.vue
@@ -1,7 +1,6 @@
 <template>
   <div
     class="ui-layout"
-    :style="{'max-width': maxWidth, 'min-width': minWidth, 'max-height': maxHeight, 'min-height': minHeight, 'overflow-x': overflowX, 'overflow-y': overflowY}"
   >
     <slot />
   </div>
@@ -9,34 +8,7 @@
 
 <script>
 export default {
-  name: "UiLayout",
-
-  props: {
-    maxWidth: {
-      type: String,
-      default: ''
-    },
-    minWidth: {
-      type: String,
-      default: ''
-    },
-    maxHeight: {
-      type: String,
-      default: ''
-    },
-    minHeight: {
-      type: String,
-      default: ''
-    },
-    overflowX: {
-      type: String,
-      default: ''
-    },
-    overflowY: {
-      type: String,
-      default: ''
-    }
-  }
+  name: "UiLayout"
 };
 </script>
 
